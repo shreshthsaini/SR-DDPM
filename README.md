26d2fc87d22e30a9bc84ebdc953ee3c97e319275
shreshth saini <shreshthfeel@gmail.com>
2023-05-05 14:45:44 -0500

    cleaning all codes, added readme, and demo files

--- README.md
+++ README.md
@@ -1,4 +1,81 @@
-## Advanced Computer Vision Project. 
-## Denoising Diffusion Probabilistic Model for Low Level Vision Task - Super Resolution. 
+# Denoising Diffusion Probabilistic Model for Super Resolution 
 
-### Install pre-requisite libraries. 
+No official implementation of **Image Super-Resolution via Iterative Refinement(SR3)** is available. Thus we implement our version and take inspiration from existing github repos on Diffusion Models. 
+
+So far we only experiment with 4X super-resolution 16x16 --> 128x128. 
+
+## Demo : 
+
+please open and run the demo.ipynb for step by step process to run the inferece, finetuning, training, and evaluation. 
+please download the weights (Weights.zip) from [OneDrive](https://utexas-my.sharepoint.com/:u:/g/personal/krishna_durbha_my_utexas_edu/EaFytjOpdYdEqIsx6EF1Q8wBm2BlsrUdeS7J6qRXmdJ_yg?e=g7ojZS). Unzip it and copy the folders and paste them in the current location or location of this Readme file.
+
+### setup
+create a virtualenv/conda is preferred. 
+```python
+pip install -r requirement.txt
+```
+
+### Dataset for demo 
+
+We have create a separate folder  "/dataset" which should contain all datasets. For demo we have added few samples for all reference dataset. 
+
+Also, if you have your own data and wants to try model on that please prepare the data first with:
+```python
+python data/prepare_data.py  --path [dataset root]  --out [output root] --size 16,128
+```
+Make sure to follow the same structure as provided in '/dataset' folder 
+
+Update the paths in config file.
+
+
+### Model Weights and Experiments
+
+Please check following folder for all model weights.
+```path
+deno_model_weight
+```
+
+Although Demo provides proper guidance; if need to change or play around with finetuning, one can change the path for pretrained model in config file here:
+
+```python
+"resume_state": [your pretrained path]
+```
+
+
+### Finetuning
+config file have path for pretrained model. for details check demo.ipynb
+
+```python
+python sr.py -p train -c [config path] #config/sr_sr3_16_128_AnimeF.json
+```
+
+### Zeroshot
+Navigate to DDNM folder and execute the following command. for details check demo.ipynb
+
+```python
+python main.py --ni --simplified --config imagenet_256.yml --path_y 'exp/datasets/imagenet/imagenet' --eta 0.85 --deg 'sr_averagepooling' --deg_scale 4.0 --sigma_y 0 -i demo
+```
+
+### Infer
+
+```python
+python infer.py -c [config path] #config/sr_sr3_16_128_AnimeF.json
+```
+
+### Evaluation
+```python
+python eval.py -p [path to results] #misc_results/sr_ffhq_AimeF_Finetuned_infer_celebhq_Iter_100K_results_230422_203929/results
+```
+
+
+## Based on
+
+- https://github.com/openai/guided-diffusion/tree/22e0df8183507e13a7813f8d38d51b072ca1e67c
+- https://github.com/lucidrains/denoising-diffusion-pytorch
+- https://github.com/rosinality/denoising-diffusion-pytorch
+
+## Authors
+
+Yu-Chih Chen \
+Shreshth Saini \
+Krishna Srikar Durbha 
---
+++ model/model.py
@@ -0,0 +1,208 @@
+import logging
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import os
+import model.networks as networks
+logger = logging.getLogger('base')
+
+# defining structure
+class BaseModel():
+    def __init__(self, opt):
+        self.opt = opt
+        self.device = torch.device(
+            'cuda' if opt['gpu_ids'] is not None else 'cpu')
+        self.begin_step = 0
+        self.begin_epoch = 0
+
+    def feed_data(self, data):
+        pass
+
+    def optimize_parameters(self):
+        pass
+
+    def get_current_visuals(self):
+        pass
+
+    def get_current_losses(self):
+        pass
+
+    def print_network(self):
+        pass
+
+    def set_device(self, x):
+        if isinstance(x, dict):
+            for key, item in x.items():
+                if item is not None:
+                    x[key] = item.to(self.device)
+        elif isinstance(x, list):
+            for item in x:
+                if item is not None:
+                    item = item.to(self.device)
+        else:
+            x = x.to(self.device)
+        return x
+
+    def get_network_description(self, network):
+        '''Get the string and total parameters of the network'''
+        if isinstance(network, nn.DataParallel):
+            network = network.module
+        s = str(network)
+        n = sum(map(lambda x: x.numel(), network.parameters()))
+        return s, n
+
+#main class
+class DDPM(BaseModel):
+    def __init__(self, opt):
+        super(DDPM, self).__init__(opt)
+        # define network and load pretrained models
+        self.netG = self.set_device(networks.define_G(opt))
+        self.schedule_phase = None
+
+        # set loss and load resume state
+        self.set_loss()
+        self.set_new_noise_schedule(
+            opt['model']['beta_schedule']['train'], schedule_phase='train')
+        if self.opt['phase'] == 'train':
+            self.netG.train()
+            # find the parameters to optimize
+            if opt['model']['finetune_norm']:
+                optim_params = []
+                for k, v in self.netG.named_parameters():
+                    v.requires_grad = False
+                    if k.find('transformer') >= 0:
+                        v.requires_grad = True
+                        v.data.zero_()
+                        optim_params.append(v)
+                        logger.info(
+                            'Params [{:s}] initialized to 0 and will optimize.'.format(k))
+            else:
+                optim_params = list(self.netG.parameters())
+
+            self.optG = torch.optim.Adam(
+                optim_params, lr=opt['train']["optimizer"]["lr"])
+            self.log_dict = OrderedDict()
+        self.load_network()
+        #self.print_network()
+
+    def feed_data(self, data):
+        self.data = self.set_device(data)
+
+    def optimize_parameters(self):
+        self.optG.zero_grad()
+        l_pix = self.netG(self.data)
+        # need to average in multi-gpu
+        b, c, h, w = self.data['HR'].shape
+        l_pix = l_pix.sum()/int(b*c*h*w)
+        l_pix.backward()
+        self.optG.step()
+
+        # set log
+        self.log_dict['l_pix'] = l_pix.item()
+
+    def test(self, continous=False):
+        self.netG.eval()
+        with torch.no_grad():
+            if isinstance(self.netG, nn.DataParallel):
+                self.SR = self.netG.module.super_resolution(
+                    self.data['SR'], continous)
+            else:
+                self.SR = self.netG.super_resolution(
+                    self.data['SR'], continous)
+        self.netG.train()
+
+    def sample(self, batch_size=1, continous=False):
+        self.netG.eval()
+        with torch.no_grad():
+            if isinstance(self.netG, nn.DataParallel):
+                self.SR = self.netG.module.sample(batch_size, continous)
+            else:
+                self.SR = self.netG.sample(batch_size, continous)
+        self.netG.train()
+
+    def set_loss(self):
+        if isinstance(self.netG, nn.DataParallel):
+            self.netG.module.set_loss(self.device)
+        else:
+            self.netG.set_loss(self.device)
+
+    def set_new_noise_schedule(self, schedule_opt, schedule_phase='train'):
+        if self.schedule_phase is None or self.schedule_phase != schedule_phase:
+            self.schedule_phase = schedule_phase
+            if isinstance(self.netG, nn.DataParallel):
+                self.netG.module.set_new_noise_schedule(
+                    schedule_opt, self.device)
+            else:
+                self.netG.set_new_noise_schedule(schedule_opt, self.device)
+
+    def get_current_log(self):
+        return self.log_dict
+
+    def get_current_visuals(self, need_LR=True, sample=False):
+        out_dict = OrderedDict()
+        if sample:
+            out_dict['SAM'] = self.SR.detach().float().cpu()
+        else:
+            out_dict['SR'] = self.SR.detach().float().cpu()
+            out_dict['INF'] = self.data['SR'].detach().float().cpu()
+            out_dict['HR'] = self.data['HR'].detach().float().cpu()
+            if need_LR and 'LR' in self.data:
+                out_dict['LR'] = self.data['LR'].detach().float().cpu()
+            else:
+                out_dict['LR'] = out_dict['INF']
+        return out_dict
+
+    def print_network(self):
+        s, n = self.get_network_description(self.netG)
+        if isinstance(self.netG, nn.DataParallel):
+            net_struc_str = '{} - {}'.format(self.netG.__class__.__name__,
+                                             self.netG.module.__class__.__name__)
+        else:
+            net_struc_str = '{}'.format(self.netG.__class__.__name__)
+
+        logger.info(
+            'Network G structure: {}, with parameters: {:,d}'.format(net_struc_str, n))
+        logger.info(s)
+
+    def save_network(self, epoch, iter_step):
+        gen_path = os.path.join(
+            self.opt['path']['checkpoint'], 'I{}_E{}_gen.pth'.format(iter_step, epoch))
+        opt_path = os.path.join(
+            self.opt['path']['checkpoint'], 'I{}_E{}_opt.pth'.format(iter_step, epoch))
+        # gen
+        network = self.netG
+        if isinstance(self.netG, nn.DataParallel):
+            network = network.module
+        state_dict = network.state_dict()
+        for key, param in state_dict.items():
+            state_dict[key] = param.cpu()
+        torch.save(state_dict, gen_path)
+        # opt
+        opt_state = {'epoch': epoch, 'iter': iter_step,
+                     'scheduler': None, 'optimizer': None}
+        opt_state['optimizer'] = self.optG.state_dict()
+        torch.save(opt_state, opt_path)
+
+        logger.info(
+            'Saved model in [{:s}] ...'.format(gen_path))
+
+    def load_network(self):
+        load_path = self.opt['path']['resume_state']
+        if load_path is not None:
+            logger.info(
+                'Loading pretrained model for G [{:s}] ...'.format(load_path))
+            gen_path = '{}_gen.pth'.format(load_path)
+            opt_path = '{}_opt.pth'.format(load_path)
+            # gen
+            network = self.netG
+            if isinstance(self.netG, nn.DataParallel):
+                network = network.module
+            network.load_state_dict(torch.load(
+                gen_path), strict=(not self.opt['model']['finetune_norm']))
+            if self.opt['phase'] == 'train':
+                # optimizer
+                opt = torch.load(opt_path)
+                self.optG.load_state_dict(opt['optimizer'])
+                self.begin_step = opt['iter']
+                self.begin_epoch = opt['epoch']
